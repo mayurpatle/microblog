272ae7bbe5e840246c9132d22ed01ae443fe12a5
Mayur <mayurpatle108@gmail.com>
2024-04-22 16:15:09 +0530

    initial

---
+++ Readme.txt
@@ -0,0 +1,3 @@
+1.  i  learned  how to render  the html  file in python app 
+2.  i learned how to user the jinja 2  expression in  my flask app 
+3.  learned  how to connect  databse using pymongo   
---
+++ app.py
@@ -0,0 +1,47 @@
+import datetime
+from flask import Flask , render_template  , request
+from  pymongo import MongoClient  
+
+
+
+
+
+
+
+app  =  Flask(__name__) 
+client  =  MongoClient("mongodb://localhost:27017/")
+app.db = client.Microblog
+
+ 
+
+entries  =  []   
+
+
+@app.route("/" , methods = ["GET"  , "POST"])
+def Home():
+    
+    
+    if request.method ==  "POST":
+        entry_content  = request.form.get("content")
+        format   =  datetime.datetime.today().strftime("%b -%d")
+        entries.append((entry_content   , format))
+        app.db.entries.insert_one({
+            "content" : entry_content  , 
+            "Date" :  format  
+        })
+        
+        
+        
+    entries_with_date = [ 
+                         ( entry[0]  ,  
+                          entry[1]  , 
+                          
+                          
+                          
+                          )
+                         
+                         for  entry  in  entries 
+                         
+    ]
+    return render_template("home.html"  ,   entries  = entries_with_date )
+
